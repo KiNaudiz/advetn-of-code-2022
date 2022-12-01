b4752dc5082a37f4eb5f8b33c2d6d220d3fbd58e
Clemens Kurzenberg <clemens@kurzenberg.org>
2022-12-01 12:29:33 +0100

    feat: solve day 1, part 1

---
+++ 01-01/01.lisp
@@ -0,0 +1,30 @@
+(defparameter *input-file* "input")
+
+(defun read-all-lines (filename)
+  (with-open-file (in filename)
+    (loop for line = (read-line in nil nil)
+          while line
+          collect line)))
+
+(defun split-at (sep list)
+  (cond ((null list) '())
+        ((equal (first list) sep) (cons '() (split-at sep (rest list))))
+        (t (let ((r (split-at sep (rest list))))
+             (cons (cons (first list) (first r)) (rest r))))))
+
+(defun read-input-calories (filename)
+  (let* ((lines (read-all-lines filename))
+         (nums (mapcar (lambda (l) (parse-integer l :junk-allowed t))
+                       lines)))
+    (split-at 'nil nums)))
+
+(defun sum-calories (cals)
+  (apply '+ cals))
+
+(defun max-calories (cals)
+  (reduce #'max cals))
+
+(defun find-max-cals-elf (filename)
+  (max-calories
+   (mapcar #'sum-calories
+           (read-input-calories filename))))
